d172e4eda4822969925efd1803611814a9b8ff90
rotvein <vadimgerasenko3002@gmail.com>
2024-05-16 19:33:26 +0300

    Updates

--- bundle.js
+++ bundle.js
@@ -26,7 +26,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style/style.scss */ \"./src/style/style.scss\");\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://orbiting-icon-animation/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style/style.scss */ \"./src/style/style.scss\");\n\n\n//# sourceURL=webpack://orbiting-icon-animation/./src/index.js?");
 
 /***/ })
 
